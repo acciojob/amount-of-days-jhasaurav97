db4c1100fab0cc4b3c7a14a8c5f96bdcc5b2469a
Saurav <jhasaurav593@gmail.com>
2025-05-14 23:49:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  if (year > 1 && year < 9999) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+      return 366;
+    } else {
+      return 365;
+    }
+  } else {
+    return "Please enter valid input";
+  }
+}
